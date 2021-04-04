11673c7f559a3bee087dc5bc17a770b463d150d0
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-04-04 17:35:50 +0200

    Add BinaryReader

---
+++ Nbt.Tests/BinaryReaderTests.cs
@@ -0,0 +1,247 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nbt.Tags;
+using System;
+using System.IO;
+
+namespace Nbt.Tests {
+    [TestClass]
+    public class BinaryReaderTests {
+        [TestMethod]
+        public void BinaryReader_ShouldReadByteCorrectly() {
+            byte[] data = new byte[] { 0x80 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            sbyte result = binaryReader.ReadByte();
+            Assert.AreEqual((sbyte)-128, result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ReadByteShouldThrowException_BecauseEndOfStreamWasReached() {
+            byte[] data = Array.Empty<byte>();
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadByte());
+        }
+
+        [TestMethod]
+        public void BinaryReader_ShouldReadUnsignedByteCorrectly() {
+            byte[] data = new byte[] { 0x80 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            byte result = binaryReader.ReadUnsignedByte();
+            Assert.AreEqual((byte)128, result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ReadUnsignedByteShouldThrowException_BecauseEndOfStreamWasReached() {
+            byte[] data = Array.Empty<byte>();
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadUnsignedByte());
+        }
+
+        [TestMethod]
+        public void BinaryReader_ShouldReadShortCorrectly() {
+            byte[] data = new byte[] { 0x80, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            short result = binaryReader.ReadShort();
+            Assert.AreEqual((short)-32768, result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ReadShortShouldThrowException_BecauseEndOfStreamWasReached() {
+            byte[] data = new byte[] { 0x80 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadShort());
+        }
+
+        [TestMethod]
+        public void BinaryReader_ShouldReadUnsignedShortCorrectly() {
+            byte[] data = new byte[] { 0x80, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            ushort result = binaryReader.ReadUnsignedShort();
+            Assert.AreEqual((ushort)32768, result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ReadUnsignedShortShouldThrowException_BecauseEndOfStreamWasReached() {
+            byte[] data = new byte[] { 0x80 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadUnsignedShort());
+        }
+
+        [TestMethod]
+        public void BinaryReader_ShouldReadIntCorrectly() {
+            byte[] data = new byte[] { 0x80, 0x00, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            int result = binaryReader.ReadInt();
+            Assert.AreEqual(-2147483648, result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ReadIntShouldThrowException_BecauseEndOfStreamWasReached() {
+            byte[] data = new byte[] { 0x80, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadInt());
+        }
+
+        [TestMethod]
+        public void BinaryReader_ShouldReadUnsignedIntCorrectly() {
+            byte[] data = new byte[] { 0x80, 0x00, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            uint result = binaryReader.ReadUnsignedInt();
+            Assert.AreEqual(2147483648, result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ReadUnsignedIntShouldThrowException_BecauseEndOfStreamWasReached() {
+            byte[] data = new byte[] { 0x80, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadUnsignedInt());
+        }
+
+        [TestMethod]
+        public void BinaryReader_ShouldReadLongCorrectly() {
+            byte[] data = new byte[] { 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            long result = binaryReader.ReadLong();
+            Assert.AreEqual(-9223372036854775808L, result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ReadLongShouldThrowException_BecauseEndOfStreamWasReached() {
+            byte[] data = new byte[] { 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadLong());
+        }
+
+        [TestMethod]
+        public void BinaryReader_ShouldReadUnsignedLongCorrectly() {
+            byte[] data = new byte[] { 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            ulong result = binaryReader.ReadUnsignedLong();
+            Assert.AreEqual(9223372036854775808L, result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ReadUnsignedLongShouldThrowException_BecauseEndOfStreamWasReached() {
+            byte[] data = new byte[] { 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadUnsignedLong());
+        }
+
+        [TestMethod]
+        public void BinaryReader_ShouldReadFloatCorrectly() {
+            byte[] data = new byte[] { 0xBF, 0xC0, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            float result = binaryReader.ReadFloat();
+            Assert.AreEqual(-1.5f, result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ReadFloatShouldThrowException_BecauseEndOfStreamWasReached() {
+            byte[] data = new byte[] { 0xBF, 0xC0, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadFloat());
+        }
+
+        [TestMethod]
+        public void BinaryReader_ShouldReadDoubleCorrectly() {
+            byte[] data = new byte[] { 0xBF, 0xF8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            double result = binaryReader.ReadDouble();
+            Assert.AreEqual(-1.5d, result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ReadDoubleShouldThrowException_BecauseEndOfStreamWasReached() {
+            byte[] data = new byte[] { 0xBF, 0xF8, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadDouble());
+        }
+
+        [TestMethod]
+        public void BinaryReader_ShouldReadTagTypeCorrectly() {
+            byte[] data = new byte[] { 0x01 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            TagType result = binaryReader.ReadTagType();
+            Assert.AreEqual(TagType.Byte, result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ReadTagTypeShouldThrowException_BecauseEndOfStreamWasReached() {
+            byte[] data = Array.Empty<byte>();
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadTagType());
+        }
+
+        [TestMethod]
+        public void BinaryReader_ShouldReadStringCorrectly() {
+            byte[] data = new byte[] { 0x00, 0x03, 0x66, 0x6F, 0x6F };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            string result = binaryReader.ReadString();
+            Assert.AreEqual("foo", result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ShouldReadStringCorrectly_WithComplexUTF8() {
+            byte[] data = new byte[] { 0x00, 0x05, 0x66, 0xC3, 0xB6, 0xC3, 0xB6 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            string result = binaryReader.ReadString();
+            Assert.AreEqual("föö", result);
+        }
+
+        [TestMethod]
+        public void BinaryReader_ReadStringShouldThrowException_BecauseEndOfStreamWasReached() {
+            byte[] data = new byte[] { 0x00, 0x05 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using BinaryReader binaryReader = new BinaryReader(memoryStream);
+
+            Assert.ThrowsException<EndOfStreamException>(() => binaryReader.ReadString());
+        }
+    }
+}
---
+++ Nbt/BinaryReader.cs
@@ -0,0 +1,106 @@
+﻿using Nbt.Tags;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Nbt {
+    class BinaryReader : IDisposable {
+        private readonly Stream Stream;
+        private readonly byte[] Buffer = new byte[sizeof(long)];
+
+        public BinaryReader(Stream stream) {
+            Stream = stream;
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public sbyte ReadByte() {
+            FillBuffer(1);
+            return (sbyte)Buffer[0];
+        }
+
+        public byte ReadUnsignedByte() {
+            FillBuffer(1);
+            return Buffer[0];
+        }
+
+        public short ReadShort() {
+            FillBuffer(2);
+            return (short)(Buffer[0] << 8 | Buffer[1]);
+        }
+
+        public ushort ReadUnsignedShort() {
+            FillBuffer(2);
+            return (ushort)(Buffer[0] << 8 | Buffer[1]);
+        }
+
+        public int ReadInt() {
+            FillBuffer(4);
+            return Buffer[0] << 24 | Buffer[1] << 16 | Buffer[2] << 8 | Buffer[3];
+        }
+
+        public uint ReadUnsignedInt() {
+            FillBuffer(4);
+            return (uint)(Buffer[0] << 24 | Buffer[1] << 16 | Buffer[2] << 8 | Buffer[3]);
+        }
+
+        public long ReadLong() {
+            FillBuffer(8);
+            uint upper = (uint)(Buffer[0] << 24 | Buffer[1] << 16 | Buffer[2] << 8 | Buffer[3]);
+            uint lower = (uint)(Buffer[4] << 24 | Buffer[5] << 16 | Buffer[6] << 8 | Buffer[7]);
+            return (long)((ulong)upper << 32 | lower);
+        }
+
+        public ulong ReadUnsignedLong() {
+            FillBuffer(8);
+            uint upper = (uint)(Buffer[0] << 24 | Buffer[1] << 16 | Buffer[2] << 8 | Buffer[3]);
+            uint lower = (uint)(Buffer[4] << 24 | Buffer[5] << 16 | Buffer[6] << 8 | Buffer[7]);
+            return (ulong)upper << 32 | lower;
+        }
+
+        public unsafe float ReadFloat() {
+            int i = ReadInt();
+            return *(float*)&i;
+        }
+
+        public unsafe double ReadDouble() {
+            long l = ReadLong();
+            return *(double*)&l;
+        }
+
+        public TagType ReadTagType() {
+            return (TagType)ReadByte();
+        }
+
+        public string ReadString() {
+            ushort length = ReadUnsignedShort();
+            byte[] bytes = ReadBytes(length);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        private byte[] ReadBytes(int length) {
+            byte[] buffer = new byte[length];
+            int bytesRead = Stream.Read(buffer, 0, length);
+            if (bytesRead < length) {
+                throw new EndOfStreamException();
+            }
+            return buffer;
+        }
+
+        private void FillBuffer(int length) {
+            int bytesRead = Stream.Read(Buffer, 0, length);
+            if (bytesRead < length) {
+                throw new EndOfStreamException();
+            }
+        }
+
+        private void Dispose(bool disposing) {
+            if (disposing) {
+                Stream.Dispose();
+            }
+        }
+    }
+}
