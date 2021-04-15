a2efa2003f8eda23e6cd4d73abb5630e0d342ace
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-04-15 17:05:49 +0200

    Add BinaryWriter

---
+++ Nbt.Tests/BinaryWriterTests.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nbt.Tags;
+using System;
+using System.IO;
+
+namespace Nbt.Tests {
+    [TestClass]
+    public class BinaryWriterTests {
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteByteCorrectly() {
+            sbyte value = -128;
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x80 }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteUnsignedByteCorrectly() {
+            byte value = 128;
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x80 }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteShortCorrectly() {
+            short value = -32768;
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x80, 0x00 }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteUnsignedShortCorrectly() {
+            ushort value = 32768;
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x80, 0x00 }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteIntCorrectly() {
+            int value = -2147483648;
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x80, 0x00, 0x00, 0x00 }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteUnsignedIntCorrectly() {
+            uint value = 2147483648;
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x80, 0x00, 0x00, 0x00 }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteLongCorrectly() {
+            long value = -9223372036854775808L;
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteUnsignedLongCorrectly() {
+            ulong value = 9223372036854775808L;
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteFloatCorrectly() {
+            float value = -1.5f;
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0xBF, 0xC0, 0x00, 0x00 }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteDoubleCorrectly() {
+            double value = -1.5d;
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0xBF, 0xF8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteTagTypeCorrectly() {
+            TagType value = TagType.Byte;
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x01 }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteStringCorrectly() {
+            string value = "foo";
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x00, 0x03, 0x66, 0x6F, 0x6F }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_ShouldWriteStringCorrectly_WithComplexUTF8() {
+            string value = "föö";
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(value);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x00, 0x05, 0x66, 0xC3, 0xB6, 0xC3, 0xB6 }, data);
+        }
+
+        [TestMethod]
+        public void BinaryWriter_WriteStringShouldThrowException_BecauseStringIsTooLong() {
+            string value = new string('a', 65536);
+            using MemoryStream memoryStream = new MemoryStream();
+            using BinaryWriter binaryWriter = new BinaryWriter(memoryStream);
+
+            Assert.ThrowsException<InvalidOperationException>(() => binaryWriter.Write(value));
+        }
+    }
+}
---
+++ Nbt/BinaryWriter.cs
@@ -0,0 +1,119 @@
+﻿using Nbt.Tags;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Nbt {
+    class BinaryWriter : IDisposable {
+        private readonly Stream Stream;
+        private readonly byte[] Buffer = new byte[sizeof(long)];
+
+        public BinaryWriter(Stream stream) {
+            Stream = stream;
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Write(sbyte b) {
+            Buffer[0] = (byte)b;
+            WriteBuffer(1);
+        }
+
+        public void Write(byte b) {
+            Buffer[0] = b;
+            WriteBuffer(1);
+        }
+
+        public void Write(short s) {
+            Buffer[0] = (byte)(s >> 8 & 0xFF);
+            Buffer[1] = (byte)(s & 0xFF);
+            WriteBuffer(2);
+        }
+
+        public void Write(ushort s) {
+            Buffer[0] = (byte)(s >> 8 & 0xFF);
+            Buffer[1] = (byte)(s & 0xFF);
+            WriteBuffer(2);
+        }
+
+        public void Write(int i) {
+            Buffer[0] = (byte)(i >> 24 & 0xFF);
+            Buffer[1] = (byte)(i >> 16 & 0xFF);
+            Buffer[2] = (byte)(i >> 8 & 0xFF);
+            Buffer[3] = (byte)(i & 0xFF);
+            WriteBuffer(4);
+        }
+
+        public void Write(uint i) {
+            Buffer[0] = (byte)(i >> 24 & 0xFF);
+            Buffer[1] = (byte)(i >> 16 & 0xFF);
+            Buffer[2] = (byte)(i >> 8 & 0xFF);
+            Buffer[3] = (byte)(i & 0xFF);
+            WriteBuffer(4);
+        }
+
+        public void Write(long l) {
+            Buffer[0] = (byte)(l >> 56 & 0xFF);
+            Buffer[1] = (byte)(l >> 48 & 0xFF);
+            Buffer[2] = (byte)(l >> 40 & 0xFF);
+            Buffer[3] = (byte)(l >> 32 & 0xFF);
+            Buffer[4] = (byte)(l >> 24 & 0xFF);
+            Buffer[5] = (byte)(l >> 16 & 0xFF);
+            Buffer[6] = (byte)(l >> 8 & 0xFF);
+            Buffer[7] = (byte)(l & 0xFF);
+            WriteBuffer(8);
+        }
+
+        public void Write(ulong l) {
+            Buffer[0] = (byte)(l >> 56 & 0xFF);
+            Buffer[1] = (byte)(l >> 48 & 0xFF);
+            Buffer[2] = (byte)(l >> 40 & 0xFF);
+            Buffer[3] = (byte)(l >> 32 & 0xFF);
+            Buffer[4] = (byte)(l >> 24 & 0xFF);
+            Buffer[5] = (byte)(l >> 16 & 0xFF);
+            Buffer[6] = (byte)(l >> 8 & 0xFF);
+            Buffer[7] = (byte)(l & 0xFF);
+            WriteBuffer(8);
+        }
+
+        public unsafe void Write(float f) {
+            int i = *(int*)&f;
+            Write(i);
+        }
+
+        public unsafe void Write(double d) {
+            long l = *(long*)&d;
+            Write(l);
+        }
+
+        public void Write(TagType tagType) {
+            Write((sbyte)tagType);
+        }
+
+        public void Write(string s) {
+            byte[] bytes = Encoding.UTF8.GetBytes(s);
+            if (bytes.Length > ushort.MaxValue) {
+                throw new InvalidOperationException("The number of bytes in the string was larger than the maximum (65535)");
+            }
+            Write((ushort)bytes.Length);
+            Write(bytes);
+        }
+
+        private void Write(byte[] bytes) {
+            Stream.Write(bytes, 0, bytes.Length);
+        }
+
+        private void WriteBuffer(int length) {
+            Stream.Write(Buffer, 0, length);
+        }
+
+        private void Dispose(bool disposing) {
+            if (disposing) {
+                Stream.Dispose();
+            }
+        }
+    }
+}
