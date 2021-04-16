88618de5490603d4bb7ed14e259ccbc79eaa6952
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-04-16 12:23:21 +0200

    Add NbtWriter

---
+++ Nbt.Tests/NbtWriterTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nbt.Tags;
+using System.IO;
+
+namespace Nbt.Tests {
+    [TestClass]
+    public class NbtWriterTests {
+        [TestMethod]
+        public void NbtWriter_WritesCorrectly() {
+            NbtRoot nbtRoot = new NbtRoot("foo", new ByteTag(127));
+            using MemoryStream memoryStream = new MemoryStream();
+            using NbtWriter nbtWriter = new NbtWriter(memoryStream);
+
+            nbtWriter.Write(nbtRoot);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x01, 0x00, 0x03, 0x66, 0x6F, 0x6F, 0x7F }, data);
+        }
+    }
+}
---
+++ Nbt/NbtWriter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Nbt {
+    public class NbtWriter : IDisposable {
+        private readonly Stream Stream;
+
+        public NbtWriter(Stream stream) {
+            Stream = stream;
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Write(NbtRoot nbtRoot) {
+            using BinaryWriter binaryWriter = new BinaryWriter(Stream);
+            binaryWriter.Write(nbtRoot.Data.GetTagType());
+            binaryWriter.Write(nbtRoot.RootName);
+            nbtRoot.Data.Write(binaryWriter);
+        }
+
+        private void Dispose(bool disposing) {
+            if (disposing) {
+                Stream.Dispose();
+            }
+        }
+    }
+}
