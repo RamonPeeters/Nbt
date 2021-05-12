ec16959133fb28c7969bff2829dcdf3bf1b9f168
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-05-12 11:59:38 +0200

    Use NbtCompression in NbtWriter

--- Nbt.Tests/NbtWriterTests.cs
+++ Nbt.Tests/NbtWriterTests.cs
@@ -9,12 +9,36 @@ namespace Nbt.Tests {
         public void NbtWriter_WritesCorrectly() {
             NbtRoot nbtRoot = new NbtRoot("foo", new ByteTag(127));
             using MemoryStream memoryStream = new MemoryStream();
-            using NbtWriter nbtWriter = new NbtWriter(memoryStream, true);
+            using NbtWriter nbtWriter = new NbtWriter(memoryStream, true, NbtCompression.None);
 
             nbtWriter.Write(nbtRoot);
             byte[] data = memoryStream.ToArray();
 
             CollectionAssert.AreEqual(new byte[] { 0x01, 0x00, 0x03, 0x66, 0x6F, 0x6F, 0x7F }, data);
+        }
+
+        [TestMethod]
+        public void NbtWriter_WritesCorrectly_WithGZipCompression() {
+            NbtRoot nbtRoot = new NbtRoot("foo", new ByteTag(127));
+            using MemoryStream memoryStream = new MemoryStream();
+            using NbtWriter nbtWriter = new NbtWriter(memoryStream, true, NbtCompression.GZip);
+
+            nbtWriter.Write(nbtRoot);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x1F, 0x8B, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A, 0x62, 0x64, 0x60, 0x4E, 0xCB, 0xCF, 0xAF, 0x07, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x03, 0x00, 0x1D, 0x91, 0xA2, 0x03, 0x07, 0x00, 0x00, 0x00 }, data);
+        }
+
+        [TestMethod]
+        public void NbtWriter_WritesCorrectly_WithZlibCompression() {
+            NbtRoot nbtRoot = new NbtRoot("foo", new ByteTag(127));
+            using MemoryStream memoryStream = new MemoryStream();
+            using NbtWriter nbtWriter = new NbtWriter(memoryStream, true, NbtCompression.ZLib);
+
+            nbtWriter.Write(nbtRoot);
+            byte[] data = memoryStream.ToArray();
+
+            CollectionAssert.AreEqual(new byte[] { 0x78, 0x9C, 0x62, 0x64, 0x60, 0x4E, 0xCB, 0xCF, 0xAF, 0x07, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x03, 0x00, 0x04, 0x5F, 0x01, 0xC8 }, data);
         }
     }
 }
--- Nbt/NbtWriter.cs
+++ Nbt/NbtWriter.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.IO;
+using System.IO.Compression;
 
 namespace Nbt {
     public class NbtWriter : IDisposable {
+        private const int BufferSize = 8192;
+
         private readonly Stream Stream;
+        private readonly NbtCompression Compression;
         private readonly bool BigEndian;
 
-        public NbtWriter(Stream stream, bool bigEndian) {
+        public NbtWriter(Stream stream, bool bigEndian, NbtCompression compression) {
             Stream = stream;
             BigEndian = bigEndian;
+            Compression = compression;
         }
 
-        public NbtWriter(Stream stream) : this(stream, true) {}
+        public NbtWriter(Stream stream, bool bigEndian) : this(stream, bigEndian, NbtCompression.None) {}
+
+        public NbtWriter(Stream stream) : this(stream, true, NbtCompression.None) {}
 
         public void Dispose() {
             Dispose(true);
@@ -19,7 +26,13 @@ namespace Nbt {
         }
 
         public void Write(NbtRoot nbtRoot) {
-            using BinaryWriter binaryWriter = new BinaryWriter(Stream, BigEndian);
+            using Stream stream = Compression switch {
+                NbtCompression.GZip => new GZipStream(Stream, CompressionMode.Compress, true),
+                NbtCompression.ZLib => new ZLibStream(Stream, CompressionMode.Compress, true),
+                _ => Stream
+            };
+            using BufferedStream bufferedStream = new BufferedStream(stream, BufferSize);
+            using BinaryWriter binaryWriter = new BinaryWriter(bufferedStream, BigEndian);
             binaryWriter.Write(nbtRoot.Data.GetTagType());
             binaryWriter.Write(nbtRoot.RootName);
             nbtRoot.Data.Write(binaryWriter);
