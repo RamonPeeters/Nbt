58f94a57f906a4810064476d19eff05ce6f5c93c
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-05-17 13:47:24 +0200

    Add a few more tests for compression auto detection in NbtReader

--- Nbt.Tests/NbtReaderTests.cs
+++ Nbt.Tests/NbtReaderTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Nbt.Tags;
+using System;
 using System.IO;
 
 namespace Nbt.Tests {
@@ -58,7 +59,18 @@ namespace Nbt.Tests {
         }
 
         [TestMethod]
-        public void NbtReader_ReadsCorrectRootName_WithCompressionAutoDetection() {
+        public void NbtReader_ReadsCorrectRootName_WithCompressionAutoDetectionAndNoCompression() {
+            byte[] data = new byte[] { 0x01, 0x00, 0x03, 0x66, 0x6F, 0x6F, 0x00 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using NbtReader nbtReader = new NbtReader(memoryStream, true, NbtCompression.AutoDetect);
+
+            NbtRoot root = nbtReader.Read();
+            string rootName = root.RootName;
+            Assert.AreEqual("foo", rootName);
+        }
+
+        [TestMethod]
+        public void NbtReader_ReadsCorrectRootName_WithCompressionAutoDetectionAndGZipCompression() {
             byte[] data = new byte[] { 0x1F, 0x8B, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A, 0x62, 0x64, 0x60, 0x4E, 0xCB, 0xCF, 0x67, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x03, 0x00, 0xB0, 0xFD, 0x18, 0xC3, 0x07, 0x00, 0x00, 0x00 };
             using MemoryStream memoryStream = new MemoryStream(data);
             using NbtReader nbtReader = new NbtReader(memoryStream, true, NbtCompression.AutoDetect);
@@ -66,6 +78,25 @@ namespace Nbt.Tests {
             NbtRoot root = nbtReader.Read();
             string rootName = root.RootName;
             Assert.AreEqual("foo", rootName);
+        }
+
+        [TestMethod]
+        public void NbtReader_ReadsCorrectRootName_WithCompressionAutoDetectionAndZLibCompression() {
+            byte[] data = new byte[] { 0x78, 0x9C, 0x62, 0x64, 0x60, 0x4E, 0xCB, 0xCF, 0x67, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x03, 0x00, 0x03, 0xE0, 0x01, 0x49 };
+            using MemoryStream memoryStream = new MemoryStream(data);
+            using NbtReader nbtReader = new NbtReader(memoryStream, true, NbtCompression.AutoDetect);
+
+            NbtRoot root = nbtReader.Read();
+            string rootName = root.RootName;
+            Assert.AreEqual("foo", rootName);
+        }
+
+        [TestMethod]
+        public void NbtReader_ShouldThrowException_WithCompressionAutoDetection_BecauseEndOfStreamWasReached() {
+            byte[] data = Array.Empty<byte>();
+            using MemoryStream memoryStream = new MemoryStream(data);
+
+            Assert.ThrowsException<EndOfStreamException>(() => new NbtReader(memoryStream, true, NbtCompression.AutoDetect));
         }
     }
 }
