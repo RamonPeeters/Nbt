44d4ae2d69490ab6dc97f7ecde8908d9d8e8a254
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-05-17 14:15:57 +0200

    Add basic StringReader

---
+++ Nbt.Tests/Snbt/StringReaderTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nbt.Snbt;
+using System;
+
+namespace Nbt.Tests.Snbt {
+    [TestClass]
+    public class StringReaderTests {
+        [TestMethod]
+        public void StringReader_CanReadShouldReturnTrue() {
+            StringReader stringReader = new StringReader("abc");
+
+            bool successful = stringReader.CanRead();
+            Assert.IsTrue(successful);
+        }
+
+        [TestMethod]
+        public void StringReader_CanReadShouldReturnFalse_BecauseNoMoreCharactersCanBeRead() {
+            StringReader stringReader = new StringReader("");
+
+            bool successful = stringReader.CanRead();
+            Assert.IsFalse(successful);
+        }
+
+        [TestMethod]
+        public void StringReader_CanReadWithLengthShouldReturnTrue() {
+            StringReader stringReader = new StringReader("abc");
+
+            bool successful = stringReader.CanRead(2);
+            Assert.IsTrue(successful);
+        }
+
+        [TestMethod]
+        public void StringReader_CanReadWithLengthShouldReturnFalse_BecauseNoMoreCharactersCanBeRead() {
+            StringReader stringReader = new StringReader("abc");
+
+            bool successful = stringReader.CanRead(5);
+            Assert.IsFalse(successful);
+        }
+
+        [TestMethod]
+        public void StringReader_ReadShouldReturnCorrectCharacter() {
+            StringReader stringReader = new StringReader("abc");
+
+            char character = stringReader.Read();
+            Assert.AreEqual('a', character);
+        }
+
+        [TestMethod]
+        public void StringReader_ReadShouldThrowException_BecauseEndOfStringWasReached() {
+            StringReader stringReader = new StringReader("");
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() => stringReader.Read());
+        }
+
+        [TestMethod]
+        public void StringReader_PeekShouldReturnCorrectCharacter() {
+            StringReader stringReader = new StringReader("abc");
+
+            char character = stringReader.Peek();
+            Assert.AreEqual('a', character);
+        }
+
+        [TestMethod]
+        public void StringReader_PeekShouldThrowException_BecauseEndOfStringWasReached() {
+            StringReader stringReader = new StringReader("");
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() => stringReader.Peek());
+        }
+
+        [TestMethod]
+        public void StringReader_SkipShouldSkipCharacter() {
+            StringReader stringReader = new StringReader("abc");
+
+            stringReader.Skip();
+            Assert.AreEqual(1, stringReader.GetCursor());
+        }
+    }
+}
---
+++ Nbt/Snbt/StringReader.cs
@@ -0,0 +1,35 @@
+﻿namespace Nbt.Snbt {
+    class StringReader {
+        private readonly string Input;
+        private int Cursor;
+
+        public StringReader(string input) {
+            Input = input;
+            Cursor = 0;
+        }
+
+        public int GetCursor() {
+            return Cursor;
+        }
+
+        public bool CanRead() {
+            return CanRead(1);
+        }
+
+        public bool CanRead(int length) {
+            return Cursor + length <= Input.Length;
+        }
+
+        public char Read() {
+            return Input[Cursor++];
+        }
+
+        public char Peek() {
+            return Input[Cursor];
+        }
+
+        public void Skip() {
+            Cursor++;
+        }
+    }
+}
