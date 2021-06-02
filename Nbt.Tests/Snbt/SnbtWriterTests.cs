693363b6349e8be03a3b477f5d3129e89ea3fd64
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-06-02 10:04:32 +0200

    Add SnbtWriter

---
+++ Nbt.Tests/Snbt/SnbtWriterTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nbt.Snbt;
+
+namespace Nbt.Tests.Snbt {
+    [TestClass]
+    public class SnbtWriterTests {
+        [TestMethod]
+        public void SnbtWriter_ShouldWriteByteCorrectly() {
+            SnbtWriter snbtWriter = new SnbtWriter();
+            snbtWriter.Write((sbyte)1);
+            Assert.AreEqual("1b", snbtWriter.ToString());
+        }
+
+        [TestMethod]
+        public void SnbtWriter_ShouldWriteShortCorrectly() {
+            SnbtWriter snbtWriter = new SnbtWriter();
+            snbtWriter.Write((short)1);
+            Assert.AreEqual("1s", snbtWriter.ToString());
+        }
+
+        [TestMethod]
+        public void SnbtWriter_ShouldWriteIntCorrectly() {
+            SnbtWriter snbtWriter = new SnbtWriter();
+            snbtWriter.Write(1);
+            Assert.AreEqual("1", snbtWriter.ToString());
+        }
+
+        [TestMethod]
+        public void SnbtWriter_ShouldWriteLongCorrectly() {
+            SnbtWriter snbtWriter = new SnbtWriter();
+            snbtWriter.Write(1L);
+            Assert.AreEqual("1L", snbtWriter.ToString());
+        }
+
+        [TestMethod]
+        public void SnbtWriter_ShouldWriteFloatCorrectly() {
+            SnbtWriter snbtWriter = new SnbtWriter();
+            snbtWriter.Write(1.5f);
+            Assert.AreEqual("1.5f", snbtWriter.ToString());
+        }
+
+        [TestMethod]
+        public void SnbtWriter_ShouldWriteDoubleCorrectly() {
+            SnbtWriter snbtWriter = new SnbtWriter();
+            snbtWriter.Write(1.5d);
+            Assert.AreEqual("1.5d", snbtWriter.ToString());
+        }
+
+        [TestMethod]
+        public void SnbtWriter_ShouldWriteCharCorrectly() {
+            SnbtWriter snbtWriter = new SnbtWriter();
+            snbtWriter.Write('a');
+            Assert.AreEqual("a", snbtWriter.ToString());
+        }
+
+        [TestMethod]
+        public void SnbtWriter_ShouldWriteStringCorrectly() {
+            SnbtWriter snbtWriter = new SnbtWriter();
+            snbtWriter.Write("foo");
+            Assert.AreEqual("foo", snbtWriter.ToString());
+        }
+    }
+}
---
+++ Nbt/Snbt/SnbtWriter.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Nbt.Snbt {
+    public class SnbtWriter {
+        private static readonly NumberFormatInfo NbtNumberFormatInfo = new NumberFormatInfo() {
+            NumberDecimalSeparator = ".",
+            PositiveInfinitySymbol = "Infinity",
+            NegativeInfinitySymbol = "-Infinity",
+            NegativeSign = "-",
+            PositiveSign = "+",
+            NaNSymbol = "NaN"
+        };
+        private readonly StringBuilder Builder;
+
+        public SnbtWriter() {
+            Builder = new StringBuilder();
+        }
+
+        public void Write(sbyte value) {
+            Builder.Append(value.ToString(NbtNumberFormatInfo)).Append('b');
+        }
+
+        public void Write(short value) {
+            Builder.Append(value.ToString(NbtNumberFormatInfo)).Append('s');
+        }
+
+        public void Write(int value) {
+            Builder.Append(value.ToString(NbtNumberFormatInfo));
+        }
+
+        public void Write(long value) {
+            Builder.Append(value.ToString(NbtNumberFormatInfo)).Append('L');
+        }
+
+        public void Write(float value) {
+            Builder.Append(value.ToString(NbtNumberFormatInfo)).Append('f');
+        }
+
+        public void Write(double value) {
+            Builder.Append(value.ToString(NbtNumberFormatInfo)).Append('d');
+        }
+
+        public void Write(char value) {
+            Builder.Append(value);
+        }
+
+        public void Write(string value) {
+            Builder.Append(value);
+        }
+
+        public override string ToString() {
+            return Builder.ToString();
+        }
+    }
+}
